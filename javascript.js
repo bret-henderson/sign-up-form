80743a08111915da88aa5eb1709ee79c62040912
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-01-19 18:08:55 -0500

    Add documentHeight function

--- javascript.js
+++ javascript.js
@@ -26,3 +26,10 @@ password.addEventListener("input", (event) => {
     confirm.setCustomValidity("");
   }
 });
+
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+ }
+ window.addEventListener('resize', documentHeight)
+ documentHeight()
