e9bf7caf3303a81e9a0f6d9aae12b990a1711fd9
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-01-19 18:14:49 -0500

    100vh fix

--- javascript.js
+++ javascript.js
@@ -26,10 +26,3 @@ password.addEventListener("input", (event) => {
     confirm.setCustomValidity("");
   }
 });
-
-const documentHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
- }
- window.addEventListener('resize', documentHeight)
- documentHeight()
