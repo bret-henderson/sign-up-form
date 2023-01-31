ac9ab587ff92af50a1afad2bc9fa2b05560d8f6b
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2023-01-31 16:32:43 -0500

    Add semicolons

--- javascript.js
+++ javascript.js
@@ -1,28 +1,28 @@
-const password = document.getElementById("password");
-const passwordError = document.querySelector('#password + .error-msg')
-const confirm = document.getElementById("confirm-password");
-const confirmError = document.querySelector('#confirm-password + .error-msg')
+const password = document.getElementById('password');
+const passwordError = document.querySelector('#password + .error-msg');
+const confirm = document.getElementById('confirm-password');
+const confirmError = document.querySelector('#confirm-password + .error-msg');
 
-confirm.addEventListener("input", (event) => {
+confirm.addEventListener('input', (event) => {
   if (confirm.value.length > 0 && confirm.value !== password.value) {
-    confirmError.textContent = '*Passwords do not match'
-    confirm.setCustomValidity("Passwords do not match!");
+    confirmError.textContent = '*Passwords do not match';
+    confirm.setCustomValidity('Passwords do not match!');
   } else {
-    passwordError.textContent = ''
-    confirmError.textContent = ''
-    password.setCustomValidity("");
-    confirm.setCustomValidity("");
+    passwordError.textContent = '';
+    confirmError.textContent = '';
+    password.setCustomValidity('');
+    confirm.setCustomValidity('');
   }
 });
 
-password.addEventListener("input", (event) => {
+password.addEventListener('input', (event) => {
   if (password.value.length > 0 && password.value !== confirm.value) {
-    confirmError.textContent = '*Passwords do not match'
-    confirm.setCustomValidity("Passwords do not match!");
+    confirmError.textContent = '*Passwords do not match';
+    confirm.setCustomValidity('Passwords do not match!');
   } else {
-    passwordError.textContent = ''
-    confirmError.textContent = ''
-    password.setCustomValidity("");
-    confirm.setCustomValidity("");
+    passwordError.textContent = '';
+    confirmError.textContent = '';
+    password.setCustomValidity('');
+    confirm.setCustomValidity('');
   }
 });
